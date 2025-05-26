daa5152e293e5780d7caf3db951bb52e73c01b83
Lionel Johnson <fariylandfuture@protonmail.com>
2025-05-26 13:22:50 +0800

    :sparkles: Refactor setup.py to load dependencies from pyproject.toml and dynamically retrieve version number

---
+++ main.py
@@ -0,0 +1,31 @@
+# coding: UTF-8
+"""
+@software: PyCharm
+@author: Lionel Johnson
+@contact: https://fairy.host
+@organization: https://github.com/FairylandFuture
+@datetime: 2025-05-26 13:12:15 UTC+08:00
+"""
+
+import tomllib
+
+def main():
+    # This is a placeholder for the main function.
+    # You can add your code here to execute when the script runs.
+    print("Hello, World!")
+
+    # read_toml()
+
+def read_toml():
+    with open("pyproject.toml", "rb") as f:
+        data = tomllib.load(f)
+
+    print(data)
+
+    print(data.get("project", {}).get("version", "0.0.0"))
+
+
+if __name__ == '__main__':
+    main()
+
+
