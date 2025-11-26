cead63c52cee10686ca10aff32bfa85fea912c93
Lionel <fairylandfuture@protonmail.com>
2025-11-26 09:14:39 +0800

    ♻️ refactor(singleton): improve thread-safety and structure of SingletonMeta

--- fairylandfuture/core/metaclass/singleton.py
+++ fairylandfuture/core/metaclass/singleton.py
@@ -7,38 +7,29 @@
 @datetime: 2024-07-03 22:53:43 UTC+08:00
 """
 
+import typing as t
 import threading
-from typing import Dict
 
 
 class SingletonMeta(type):
     """
-    Singleton pattern metaclass
+    Singleton Metaclass.
+
+    Thread-safe impl of Singleton.
     """
 
-    _instance: Dict = {}
-    _lock: threading.Lock = threading.Lock()
+    __instance: t.Dict[type, object] = {}
 
-    # @functools.lru_cache(maxsize=0)
+    def __init__(cls, name, bases, namespace, **kwargs):
+        super().__init__(name, bases, namespace, **kwargs)
+
+        cls._lock: threading.RLock = threading.RLock()
+
     def __call__(cls, *args, **kwargs):
-        """
-        Singleton pattern metaclass
+        if cls not in cls.__instance:
+            with cls._lock:
+                if cls not in cls.__instance:
+                    instance = super().__call__(*args, **kwargs)
+                    cls.__instance.update({cls: instance})
 
-        :param args: ...
-        :type args: tuple
-        :param kwargs: ...
-        :type kwargs: dict
-        :return: get instance
-        :rtype: object
-        """
-        # if not hasattr(cls, "_instance"):
-        #     setattr(cls, "_instance", superclass().__call__(*args, **kwargs))
-        #     return getattr(cls, "_instance")
-        # else:
-        #     return getattr(cls, "_instance")
-
-        with cls._lock:
-            if cls not in cls._instance:
-                instance = super().__call__(*args, **kwargs)
-                cls._instance.update({cls: instance})
-        return cls._instance.get(cls)
+        return cls.__instance.get(cls)
---
+++ test/case/__init__.py
@@ -0,0 +1,10 @@
+# coding: UTF-8
+"""
+@software: PyCharm
+@author: Lionel Johnson
+@contact: https://fairy.host
+@organization: https://github.com/FairylandFuture
+@datetime: 2025-11-25 12:25:44 UTC+08:00
+"""
+
+import typing as t
